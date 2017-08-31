f3496e28c8aedc45f0c5fc6479691eb84d2ca0e5
Anish Dalal <anishdalal95@gmail.com>
2017-08-30 22:59:28 -0700

    Player, Item

---
+++ src/Item.java
@@ -0,0 +1,48 @@
+/**
+ * Created by anishdalal on 8/30/17.
+ */
+public class Item {
+
+    private String type;
+    private String itemId;
+    private int weight;
+    private String condition;
+    private int value;
+    private String itemName;
+
+    public Item(String itemName, int weight, int value) {
+        this.itemName = itemName;
+        this.weight = weight;
+        this.value = value;
+    }
+
+    public String getItemId() {
+        return this.itemId;
+    }
+
+    public void setItemName(String newName) {
+         this.itemName = newName;
+    }
+
+    public String getItemName() {
+        return this.itemName;
+    }
+
+    public void setItemWeight(int newWeight) {
+        this.weight = newWeight;
+    }
+
+    public int getItemWeight() {
+        return this.weight;
+    }
+
+    public void setItemValue(int newValue) {
+        this.value = newValue;
+    }
+
+    public int getItemValue() {
+        return this.value;
+    }
+
+
+}
---
+++ src/Player.java
@@ -0,0 +1,65 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+/**
+ * Created by anishdalal on 8/30/17.
+ */
+
+public class Player {
+
+    private int health;
+    private ArrayList<Item> inventory;
+    private int capacity;
+    private int money;
+
+    public Player(ArrayList<Item> inventory, int capacity) {
+        this.inventory = inventory;
+        this.capacity = capacity;
+    }
+
+    public int addItemToInventory(Item item) {
+        int currWeight = calculateInventoryWeight();
+        if (currWeight + item.getItemWeight() > this.capacity) {
+            return -1;
+        }
+        inventory.add(item);
+        // this.capacity += item.getItemWeight();
+        return 0;
+    }
+
+    public int removeItemFromInventory(String name) {
+        for(int i = 0; i < inventory.size(); i ++) {
+            if(this.inventory.get(i).getItemName().equals(name)) {
+                this.inventory.remove(i);
+                return 0;
+            }
+        }
+        return -1;
+    }
+
+    public int removeItemFromInventoryWithId(String id) {
+        for(int i = 0; i < inventory.size(); i ++){
+            if(this.inventory.get(i).getItemId().equals(id)) {
+                this.inventory.remove(i);
+                return 0;
+            }
+        }
+        return -1;
+    }
+
+    public int calculateInventoryWeight() {
+        int weight = 0;
+        for (Item item: inventory) {
+            weight += item.getItemWeight();
+        }
+        return weight;
+    }
+
+    public void printOutInventory()
+    {
+        for(int i = 0; i < inventory.size(); i++)
+        {
+            System.out.println(String.format("%d - %s", i, this.inventory.get(i).getItemName()));
+        }
+    }
+}
